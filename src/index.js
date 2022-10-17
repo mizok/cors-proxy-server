a29b545d5d44d4f596ce37853b433257660efef3
mizok <karasunohina@gmail.com>
2022-10-18 04:00:26 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const cors_proxy = require('cors-anywhere');
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.HOST || 'https://cors-proxy-server.onrender.com';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 8080;
 
 
 cors_proxy.createServer({
-    originWhitelist: ['http://localhost:8080', 'http://localhost:5500'], // Allow all origins
+    originWhitelist: ['http://localhost:8080', 'http://localhost:5500', 'https://mizok.github.io'], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function () {
