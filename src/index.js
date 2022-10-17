aad61e170ab6413801ac925c9fb272d72580b3c5
mizok <karasunohina@gmail.com>
2022-10-18 04:20:33 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const cors_proxy = require('cors-anywhere');
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || 'cors-proxy-server.onrender.com';
+const host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 8080;
 
